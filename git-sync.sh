caaa682aab0b923b3c817a3ef3940399d57fc218
Silas S. Brown <ssb22@hermes.cam.ac.uk>
2019-06-25 12:34:09 +0100

    update

--- git-sync.sh
+++ git-sync.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+git pull || exit 1
 wget -N http://people.ds.cam.ac.uk/ssb22/pooler/pooler.tgz
 wget -N http://people.ds.cam.ac.uk/ssb22/pooler/script2canvas.py
+wget -N http://people.ds.cam.ac.uk/ssb22/pooler/example.txt
 tar -zxvf pooler.tgz
 git add pooler/*
 git commit -am update && git push
