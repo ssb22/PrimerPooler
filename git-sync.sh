261eda1df6c72c09b5ee10ba47f059e1cab06ac1
Silas S. Brown <ssb22@cam.ac.uk>
2020-05-03 08:12:40 +0100

    Remove git-sync.sh (updates now done by homepage/Makefile itself)

--- git-sync.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-git pull --no-edit
-wget -N http://ssb22.user.srcf.net/pooler/pooler.tgz
-wget -N http://ssb22.user.srcf.net/pooler/script2canvas.py
-wget -N http://ssb22.user.srcf.net/pooler/example.txt
-tar -zxvf pooler.tgz
-git add pooler/*
-git commit -am update && git push
