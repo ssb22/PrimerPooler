22c2e01502f42c3f27c3205f0c1940c1225d1c64
Silas S. Brown <ssb22@cam.ac.uk>
2021-01-09 12:06:06 +0000

    1.76 FreeBSD

--- freebsd/update.sh
+++ freebsd/update.sh
@@ -3,6 +3,7 @@
 # ssh freebsd pkg install portlint
 # ssh freebsd "echo 'DEVELOPER=yes' >> /etc/make.conf"
 scp Makefile distinfo pkg-descr freebsd:/usr/ports/biology/pooler/ || exit 1
-ssh freebsd 'cd /usr/ports/biology/pooler/ && svn up && portlint -A' || exit 1
+ssh freebsd 'cd /usr/ports/biology/pooler/ && svn up && rm -f distinfo && make makesum && portlint -A' || exit 1
+scp freebsd:/usr/ports/biology/pooler/distinfo . || exit 1
 ssh freebsd 'cd /usr/ports/biology/pooler/ && svn diff' > pooler.diff
 echo "pooler.diff to https://bugs.freebsd.org/bugzilla/enter_bug.cgi"
