9f92957aa830eec911d9b8a6312ffc6513dd735b
Silas S. Brown <ssb22@cam.ac.uk>
2021-08-27 12:04:23 +0100

    freebsd to git + 1.77

--- freebsd/README.md
+++ freebsd/README.md
@@ -2,6 +2,8 @@ FreeBSD port
 ============
 
 This was [committed to FreeBSD's ports tree](https://svnweb.freebsd.org/ports?view=revision&revision=555418)
-under `/usr/ports/biology/pooler` (porting discussion was on [issue 251065](https://bugs.freebsd.org/bugzilla/show_bug.cgi?id=251065))
+under `/usr/ports/biology/pooler` (porting discussion was on [issue 251065](https://bugs.freebsd.org/bugzilla/show_bug.cgi?id=251065),
+v1.77 update was
+discussed at [257975](https://bugs.freebsd.org/bugzilla/show_bug.cgi?id=257975))
 
 FreeBSD users should be able to install it via `pkg install pooler`
--- freebsd/update.sh
+++ freebsd/update.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
-
-echo "ERROR: update.sh needs changing"
-echo "FreeBSD ports have migrated from svn to git"
-exit 1
-
-# ssh freebsd svn switch https://svn.FreeBSD.org/ports/head /usr/ports
+# ssh freebsd svn switch https://svn.FreeBSD.org/ports/head /usr/ports # TODO: how to migrate this to git? (2021q3 branch is OK for now)
 # ssh freebsd pkg install portlint
 # ssh freebsd "echo 'DEVELOPER=yes' >> /etc/make.conf"
 scp Makefile distinfo pkg-descr freebsd:/usr/ports/biology/pooler/ || exit 1
-ssh freebsd 'cd /usr/ports/biology/pooler/ && svn up && rm -f distinfo && make makesum && portlint -A' || exit 1
+ssh freebsd 'cd /usr/ports/biology/pooler/ && rm -f distinfo && make makesum && portlint -A' || exit 1
 scp freebsd:/usr/ports/biology/pooler/distinfo . || exit 1
-ssh freebsd 'cd /usr/ports/biology/pooler/ && svn diff' > pooler.diff
-echo "pooler.diff to https://bugs.freebsd.org/bugzilla/enter_bug.cgi"
+ssh freebsd 'cd /usr/ports && git commit biology/pooler/* -m "Update PrimerPooler to upstream" && git format-patch --stdout -1' > pooler.mbox
+echo "pooler.mbox to https://bugs.freebsd.org/bugzilla/enter_bug.cgi (as plain-text attachment if possible)"
