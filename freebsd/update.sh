dd092c0380d4668bfe29002cc1d1865ebdec7edc
Silas S. Brown <ssb22@cam.ac.uk>
2021-09-14 07:47:53 +0100

    freebsd: don't need distinfo upstream

--- freebsd/update.sh
+++ freebsd/update.sh
@@ -2,8 +2,7 @@
 # ssh freebsd svn switch https://svn.FreeBSD.org/ports/head /usr/ports # TODO: how to migrate this to git? (2021q3 branch is OK for now)
 # ssh freebsd pkg install portlint
 # ssh freebsd "echo 'DEVELOPER=yes' >> /etc/make.conf"
-scp Makefile distinfo pkg-descr freebsd:/usr/ports/biology/pooler/ || exit 1
-ssh freebsd 'cd /usr/ports/biology/pooler/ && rm -f distinfo && make makesum && portlint -A' || exit 1
-scp freebsd:/usr/ports/biology/pooler/distinfo . || exit 1
-ssh freebsd 'cd /usr/ports && git commit biology/pooler/* -m "biology/pooler: update to '"$(grep -m 1 '^#.*Program_Version' ../pooler/version.h|cut -d ' ' -f5|tr -d '"')"'" && git format-patch --stdout -1' > pooler.mbox || exit 1
+scp Makefile pkg-descr freebsd:/usr/ports/biology/pooler/ &&
+ssh freebsd 'cd /usr/ports/biology/pooler/ && rm -f distinfo && make makesum && portlint -A && git commit * -m "biology/pooler: update to '"$(grep -m 1 '^#.*Program_Version' ../pooler/version.h|cut -d ' ' -f5|tr -d '"')"'"' &&
+ssh freebsd 'cd /usr/ports && git format-patch --stdout -1' > pooler.mbox &&
 echo "pooler.mbox to https://bugs.freebsd.org/bugzilla/enter_bug.cgi (as plain-text attachment if possible)"
